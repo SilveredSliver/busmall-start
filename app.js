59efb28a8e661641fd76a451ef897135744b8000
AaronBruce <aaron.bruce@comcast.net>
2017-04-19 07:48:48 -0700

    Got the buttons working and now attempting to onnect them to the random number generator.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function getImage(name, filePath) {
   this.name = name;
   this.filePath = filePath;
   this.numDisplayed = 0;
-  this.numbClicked = 0;
+  this.numClicked = 0;
 }
 
 //var testImage = new getImage('bag.jpg', imgDirectory);
@@ -61,7 +61,6 @@ var imgFileList = [
 
   //var foo = new selectThree(imgFileList, lastImages);
   //console.log(foo.theImages[0]);
-
   for (var j = 0; j < 25; j++) {
     var myThree = new selectThree(imgFileList, lastImages);
 
@@ -70,86 +69,117 @@ var imgFileList = [
       imgList[myThree.theImages[k]].numDisplayed++;
     };
 
+    //I need to put these  images on the  website
+    //I then need to wait for the user to click on an image
     lastImages = myThree.theImages;
+
+  var optionOneImage = imgFileList[myThree.theImages[0]];
   }
   console.log(imgList);
 
+var count1 = 0;
+var countStartB1 = document.getElementById('count1');
+countStartB1.textContent = count1;
+var b1 = document.getElementById('b1');
+function addClickEvent1() {
+  count1++
+  countStartB1.textContent = count1;
+}
+b1.addEventListener('click', addClickEvent1);
 
+var count2 = 0;
+var countStartB2 = document.getElementById('count2');
+countStartB2.textContent = count2;
+var b2 = document.getElementById('b2');
+function addClickEvent2() {
+  count2++
+  countStartB2.textContent = count2;
+}
+b2.addEventListener('click', addClickEvent2);
 
+var count3 = 0;
+var countStartB3 = document.getElementById('count3');
+countStartB3.textContent = count3;
+var b3 = document.getElementById('b3');
+function addClickEvent3() {
+  count3++
+  countStartB3.textContent = count3;
+}
+b3.addEventListener('click', addClickEvent3);
 
   // function createTable(imgFileList, imgList) {
-    var table = document.createElement('table');
-    var tableBody = document.createElement("tbody");
-    for (var i = 0; i < imgFileList.length; i++) {
+  var table = document.createElement('table');
+  var tableBody = document.createElement("tbody");
+  for (var i = 0; i < imgFileList.length; i++) {
 
-      var row = document.createElement('tr');
-      var cell = document.createElement('td');
-      cell.appendChild(document.createTextNode(imgFileList[i]));
-      row.appendChild(cell);
-      cell.appendChild(document.createTextNode(imgList[i].numDisplayed));
-    }
-    table.appendChild(tableBody);
-    document.body.appendChild(table);
+    var row = document.createElement('tr');
+    var cell = document.createElement('td');
+    cell.appendChild(document.createTextNode(imgFileList[i]));
+    row.appendChild(cell);
+    cell.appendChild(document.createTextNode(imgList[i].numDisplayed));
+  }
+  table.appendChild(tableBody);
+  document.body.appendChild(table);
   //}
 
-//var myTable = createTable(imgFileList, imgList);
-//console.log(myTable);
+  //var myTable = createTable(imgFileList, imgList);
+  //console.log(myTable);
 
-function generateRow(obj) {
-  var row = document.createElement('tr');
-  var imgName = document.createElement('td');
-  imgName.textContent = obj.name;
-  row.appendChild(imgName);
+  function generateRow(obj) {
+    var row = document.createElement('tr');
+    var imgName = document.createElement('td');
+    imgName.textContent = obj.name;
+    row.appendChild(imgName);
 
-  var numDisplayed = document.createElement('td');
-  numDisplayed.textContent = obj.numDisplayed;
-  row.appendChild(numDisplayed);
+    var numDisplayed = document.createElement('td');
+    numDisplayed.textContent = obj.numDisplayed;
+    row.appendChild(numDisplayed);
 
-  var numClicks = document.createElement('td');
-  numClicks.textContent = obj.numClicks;
-  row.appendChild(numClicks);
+    var numClicks = document.createElement('td');
+    numClicks.textContent = obj.numClicks;
+    row.appendChild(numClicks);
 
-  return row;
-};
+    return row;
+  };
 
-var myRow = generateRow(imgList[0]);
-console.log(myRow);
+  var myRow = generateRow(imgList[0]);
+  console.log(myRow);
 
-function generateAllRows(myTable, imgList) {
-  for (i = 0; i < imgList.length; i++) {
-    var nextRow = new generateRow(imgList[i]);
-    myTable.appendChild(nextRow);
-    console.log(nextRow);
+  function generateAllRows(myTable, imgList) {
+    for (i = 0; i < imgList.length; i++) {
+      var nextRow = new generateRow(imgList[i]);
+      myTable.appendChild(nextRow);
+      console.log(nextRow);
+    }
+    return myTable;
   }
-return myTable;
-}
 
 
-function generateUserSessionTable() {
-  var table = document.createElement('table');//this is a  div tag created in javascript
-  table.setAttribute('id', 'userSessionTable');
+  function generateUserSessionTable() {
+    var table = document.createElement('table');//this is a  div tag created in javascript
+    table.setAttribute('id', 'userSessionTable');
 
-  //table.setAttribute('id', 'store-report-table');
-  document.body.appendChild(table);
+    //table.setAttribute('id', 'store-report-table');
+    document.body.appendChild(table);
 
-  var headingRow = document.createElement('tr');
-  table.appendChild(headingRow);
+    var headingRow = document.createElement('tr');
+    table.appendChild(headingRow);
 
-  //add 'th' to table with text of 'Store Name'
-  var imageHeadingName = document.createElement('th');
-  imageHeadingName.textContent = 'Image Name';
-  table.appendChild(imageHeadingName);
-  var numberOfDisplays = document.createElement('th');
-  numberOfDisplays.textContent = 'Number of Displays';
-  table.appendChild(numberOfDisplays);
-  var clicks = document.createElement('th');
-  clicks.textContent = 'Number of Clicks';
-  table.appendChild(clicks);
+    //add 'th' to table with text of 'Store Name'
+    var imageHeadingName = document.createElement('th');
+    imageHeadingName.textContent = 'Image Name';
+    table.appendChild(imageHeadingName);
+    var numberOfDisplays = document.createElement('th');
+    numberOfDisplays.textContent = 'Number of Displays';
+    table.appendChild(numberOfDisplays);
+    var clicks = document.createElement('th');
+    clicks.textContent = 'Number of Clicks';
+    table.appendChild(clicks);
 
 
 
-  return table;
-}
+    return table;
+  }
 
-var userTable = generateUserSessionTable();
-userTable = generateAllRows(userTable, imgList);
+  var userTable = generateUserSessionTable();
+  userTable = generateAllRows(userTable, imgList);
